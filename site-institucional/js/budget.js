5f004edc6793be6eb9ea31f32ecf83957e3bd226
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2021-04-07 19:01:11 -0300

    corrigindo detalhes do simulador

--- site-institucional/js/budget.js
+++ site-institucional/js/budget.js
@@ -12,23 +12,26 @@ function calculateBudget() {
   } else {
 
     // PRICES R$
-    const priceArduino = 50
-    const priceSensor = 20.50
-    const priceRepeater = 300.79
-    const areaDelimitation = 500
+    const priceArduino = 50;
+    const priceSensor = 20.50;
+    const esp8266 = 20;
 
-    let numSections = meters2 / areaDelimitation;
-    let numSensors = Math.round(numSections * 4);
+    const areaDelimitation = 90;
 
-    let preco = (numSensors * (priceSensor + priceArduino)) + (numSections * priceRepeater);
-    let percent = preco * 0.3;
-    let total = preco + percent;
+    let numSections = Math.round(meters2 / areaDelimitation);
 
-    let numRepeater = Math.round(numSections) > 1 ? Math.round(numSections) + ' repetidores de sinal' : Math.round(numSections) + ' repetidor de sinal';
+    let price = (numSections * (priceArduino + priceSensor + esp8266))
 
-    document.getElementById('total').value = `Total: R$ ${total.toFixed(2).replace(".", ",")}`;
+    let labour = (price * 0.3);
+    let total = (price + labour);
 
-    details.innerHTML = `Para uma região de ${meters2}M² será utilizado um total de ${numSensors} sensores e arduinos, somados a ${numRepeater}, resultando em R$ ${preco.toFixed(2).replace('.', ',')} mais R$ ${percent.toFixed(2).replace('.', ',')} de mão de obra.`;
+    let labourFormatted = labour.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    let totalFormatted = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    let priceFormatted = price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    document.getElementById('total').value = `Total: ${totalFormatted}`;
+
+    details.innerHTML = `Para uma região de ${meters2}m² será utilizado ${numSections} sensores, arduinos e ESP8266 resultando em ${priceFormatted} mais ${labourFormatted} de mão de obra.`;
   }
 
 }
